99e710d15912de654a387f7c8e621819eaa4d807
dhawaljaiswal <dhawal@ratufa.com>
2018-10-04 18:15:11 +0530

    Update App.js

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to Ratufa NKP Developer!</Text>
-        <Text style={styles.instructions}>This is faslane demo for continuous integration.</Text>
+        <Text style={styles.instructions}>This is faslane demo with jeklins for continuous integration.</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
